0e3291c6caf56243d4293bb715b964ad8ef5e8c5
MN_Creator <Mattias.Norgren79@protonmail.com>
2023-11-15 10:11:53 +0100

    Fix notepad not loading font size correctly

--- note_tab.py
+++ note_tab.py
@@ -1,18 +1,19 @@
+import customtkinter as ctk
 from tab import Tab
 from textbox import Textbox
 
 class NoteTab(Tab):
     def create_content(self):
-        # Font size setting.
         self._create_settings()
-        self.text = Textbox(self.tab)
-        self.text.pack(fill="both", expand=True)
+        self.font = "TkDefaultFont"
+        self.text = Textbox(self.tab, font=(self.font, self.font_size_setting.value))
+        self.text.pack(fill=ctk.BOTH, expand=True)
 
     def _create_settings(self):
         options = [12, 14, 16, 18, 20]
-        font_size_setting = self.app.settings.create("notepad_font_size", 12, options=options, min_value=8, max_value=20)
-        font_size_setting.on_change = self._set_font_size
+        self.font_size_setting = self.app.settings.create("notepad_font_size", options[0], options=options)
+        self.font_size_setting.on_change = self._set_font_size
         
     def _set_font_size(self):
-        font_size = self.app.settings.get_int("notepad_font_size")
-        self.text.configure(font=("TkDefaultFont", font_size))
+        font_size = self.font_size_setting.value
+        self.text.configure(font=(self.font, font_size))
