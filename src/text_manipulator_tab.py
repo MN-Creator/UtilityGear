2fcbc8177c3fa8a11a86144e1533327518ca0d20
MN_Creator <Mattias.Norgren79@protonmail.com>
2023-11-17 18:51:49 +0100

    Add support for custom regex in text tab

--- src/text_manipulator_tab.py
+++ src/text_manipulator_tab.py
@@ -1,6 +1,5 @@
 import customtkinter as ctk
 from tab import Tab
-from entry import Entry
 from textbox import Textbox
 import re
 
@@ -26,10 +25,11 @@ class TextManipulatorTab(Tab):
         self.manipulators[sort_desc.name] = sort_desc
         unique_lines = TextManipulator("Unique", self.unique_lines)
         self.manipulators[unique_lines.name] = unique_lines
+        regex = TextManipulator("Regex", self._regex)
+        self.manipulators[regex.name] = regex
         
     def refine_text(self, text):
         text = text.strip()
-        # Remove extra whitespace.
         text = " ".join(text.split())
         return text
 
@@ -56,24 +56,32 @@ class TextManipulatorTab(Tab):
         self.input_box = Textbox(self.frame)
         self.input_box.grid(row=0, column=0, padx=2, sticky=ctk.NSEW)
         self.input_box.bind("<KeyRelease>", self.input_changed)
-        options = list(self.manipulators.keys())
-        self.option_dropdown = ctk.CTkOptionMenu(self.frame, values=options, command=self.on_option_changed)
-        self.option_dropdown.grid(row=1, column=0, padx=2, sticky=ctk.EW)
-        self.option_dropdown.set(self.current_manipulator.name)
         self.output_box = ctk.CTkTextbox(self.frame, state="disabled")
         self.output_box.grid(row=0, column=1, padx=2, sticky=ctk.NSEW)
+        options = list(self.manipulators.keys())
+        self.option_dropdown = ctk.CTkOptionMenu(self.frame, values=options, command=self.on_option_changed)
+        self.option_dropdown.grid(row=1, column=0, padx=2, sticky="SEW")
+        self.option_dropdown.set(self.current_manipulator.name)
 
     def create_main_frame(self):
         self.frame = ctk.CTkFrame(self.tab)
         self.frame.pack(fill="both", expand=True)
         self.frame.columnconfigure(0, weight=1)
         self.frame.columnconfigure(1, weight=1)
-        self.frame.rowconfigure(0, weight=1)
+        self.frame.rowconfigure(0, weight=1, minsize=250)
         self.frame.rowconfigure(1, weight=1)
 
     def on_option_changed(self, _):
         self.current_manipulator = self.manipulators[self.option_dropdown.get()]
+        self._handle_regex_inputbox()
         self.input_changed(_)
+
+    def _handle_regex_inputbox(self):
+        if self.current_manipulator.name == "Regex":
+            self._create_regex_widget()
+        elif self.regex_inputbox is not None:
+            self.regex_inputbox.destroy()
+            self.regex_inputbox = None
 
     def input_changed(self, _):
         text = self.input_box.get("1.0", "end")
@@ -86,6 +94,31 @@ class TextManipulatorTab(Tab):
     def copy_output(self):
         self.output_box.clipboard_clear()
         self.output_box.clipboard_append(self.output_box.get("1.0", "end"))
+    
+    def _create_regex_widget(self):
+        self.regex_inputbox = ctk.CTkEntry(self.frame, placeholder_text="Press enter to apply regex")
+        self.regex_inputbox.grid(row=1, column=1, padx=2, sticky="SEW")
+        self.regex_inputbox.bind("<Return>", self.input_changed)
+
+    def _regex(self, text):
+        regex_text = self.regex_inputbox.get()
+        regex_text = regex_text.strip()
+        if len(text) == 0 or len(regex_text) == 0:
+            return ""
+        regex = self._compile_regex(regex_text)
+        if regex is None:
+            return "(Invalid regex)"
+        matches = regex.findall(text)
+        if matches is None:
+            return ""
+        return matches
+
+    def _compile_regex(self, regex_text):
+        try:
+            regex = re.compile(regex_text)
+            return regex
+        except re.error:
+            return None
 
 class TextManipulator:
     def __init__(self, name, function):
