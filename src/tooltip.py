f4afcd8c9e422ea3f33a587861029af66537ff65
MN_Creator <Mattias.Norgren79@protonmail.com>
2023-11-25 11:55:59 +0100

    Create tooltip

---
+++ src/tooltip.py
@@ -0,0 +1,15 @@
+try:
+    from CTkToolTip import CTkToolTip
+except ModuleNotFoundError:
+
+    class CTkToolTip:
+        pass
+
+
+class ToolTip(CTkToolTip):
+    """Tooltip that works with an always on top root window."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Show over all other windows to display even when the root window is topmost.
+        self.attributes("-topmost", True)
